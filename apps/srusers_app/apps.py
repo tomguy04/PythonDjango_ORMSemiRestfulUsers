26260ff183e9d6d81821aa10a3391f7dd23ce663
Glen Goulter <glenrg@gmail.com>
2018-02-17 12:19:45 -0800

    loaded up just the first pae to get the form to load, needs debugging

---
+++ apps/srusers_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SrusersAppConfig(AppConfig):
+    name = 'srusers_app'
---
+++ apps/srusers_app/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+# the index function is called when root is visited
+def index(request):
+    # response = "Hello, I am your first request!"
+    return render(request,"srusers_app/index.html")
+

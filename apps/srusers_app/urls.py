4f068bb5e91d19c62bbc3be64e7c4876cb1c453f
Glen Goulter <glenrg@gmail.com>
2018-02-18 15:20:27 -0800

    everything works, but I am using session for get the edit/update data passed to the update function

--- apps/srusers_app/urls.py
+++ apps/srusers_app/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
+  url(r'^users/(?P<uid>\d+)/destroy/$', views.destroy),
+  url(r'^users/update/$', views.update),
+  url(r'^users/(?P<uid>\d+)/edit/$', views.edit),
   url(r'^users/(?P<uid>\d+)/$', views.show),
   url(r'^users/create/$', views.create),
   url(r'^users/new/$', views.new),
--- apps/srusers_app/views.py
+++ apps/srusers_app/views.py
@@ -5,30 +5,59 @@ from django.shortcuts import render, HttpResponse, redirect
 
 # the index function is called when root is visited
 def index(request):
-    # response = "Hello, I am your first request!"
-    return render(request,"srusers_app/index.html")
+    context = {
+        'all_friends' : User.objects.all()
+    }
+    
+    return render(request,"srusers_app/index.html", context)
 
 def new(request):
     return render(request,'srusers_app/adduser.html')
 
 def create(request):
     print "*********************IN CREATE"
-    u1 = User(first_name = request.POST['first_name'], last_name = request.POST['first_name'], email = request.POST['email'])
+    u1 = User(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email = request.POST['email'])
     u1.save()
     user_data = u1.id
     user_path = '/users/'+str(user_data)
     print "*********************user_path " + user_path
     return redirect(user_path)
     
-
 def show(request,uid):
-    print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    if request.method == 'GET':
+        context = {
+            'id':uid,
+            'user':User.objects.get(id=uid)
+        }
+        return render(request,"srusers_app/user_profile.html", context)
+    else:
+        request.session['id'] = uid
+        request.session['first_name'] = request.POST['first_name']
+        request.session['last_name'] = request.POST['last_name']
+        request.session['email'] = request.POST['email']
+        return redirect('/users/update')
+
+def update(request):
+    u1 = User.objects.get(id=request.session['id'] )
+    u1.first_name = request.session['first_name']
+    u1.last_name = request.session['last_name']
+    u1.email = request.session['email']
+    u1.save()
+    user_path = '/users/'+str(request.session['id'])
+    return redirect(user_path)
+        
+
+def edit(request,uid):
+    print "*****************"
     print uid
     context = {
         'id':uid,
         'user':User.objects.get(id=uid)
     }
-
- #   return render_template('user_profile.html',user = user_data[0])
-    return render(request,"srusers_app/user_profile.html", context)
     
+    return render(request,'srusers_app/edit_profile.html',context)
+
+def destroy(request,uid):
+    u = User.objects.get(id=uid)
+    u.delete()
+    return redirect('/users')
